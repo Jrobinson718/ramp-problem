f2b0d3bed6051052aa358a5d80460ae78488b779
joro2238 <131008863+Jrobinson718@users.noreply.github.com>
2023-08-27 20:42:33 +0000

    Update index.tsx

--- ramp challenge/public/src/components/InputCheckbox/index.tsx
+++ ramp challenge/public/src/components/InputCheckbox/index.tsx
@@ -1,26 +1,31 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import React, { useRef } from "react";
+import classNames from "classnames";
+import { InputCheckboxComponent } from "./types";
 
 export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  const inputIdRef = useRef(`RampInputCheckbox-${id}`);
+
+  const handleCheckboxChange = () => {
+    onChange(!checked);
+  };
 
   return (
-    <div className="RampInputCheckbox--container" data-testid={inputId}>
+    <div className="RampInputCheckbox--container" data-testid={inputIdRef.current}>
       <label
+        htmlFor={inputIdRef.current}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
       />
       <input
-        id={inputId}
+        id={inputIdRef.current}
         type="checkbox"
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={handleCheckboxChange}
       />
     </div>
-  )
-}
+  );
+};
