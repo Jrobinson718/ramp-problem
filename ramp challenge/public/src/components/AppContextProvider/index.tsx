550fb9051ce6946148b30eaa779615c4bad94e19
joro2238 <131008863+Jrobinson718@users.noreply.github.com>
2023-08-27 20:47:15 +0000

    Update index.tsx

--- ramp challenge/public/src/components/AppContextProvider/index.tsx
+++ ramp challenge/public/src/components/AppContextProvider/index.tsx
@@ -1,22 +1,38 @@
-import { useRef, useState } from "react"
-import { AppContext } from "../../utils/context"
-import { AppContextProviderComponent } from "./types"
+// Import necessary modules and components
+import React, { useRef, useState } from "react";
+// Import AppContext from the appropriate path
+// import { AppContext } from "../../utils/context";
+// Import the necessary type for AppContextProviderComponent
+// import { AppContextProviderComponent } from "./types";
 
-export const AppContextProvider: AppContextProviderComponent = ({ children }) => {
-  const cache = useRef(new Map<string, string>())
-  const [error, setError] = useState<string>("")
+// Create a basic context object since we're not using the actual context
+const AppContext = React.createContext(null);
 
+// Define the AppContextProvider component
+const AppContextProvider = ({ children }) => {
+  // Create a ref to store a cache map
+  const cache = useRef(new Map());
+
+  // Create a state to hold error messages
+  const [error, setError] = useState("");
+
+  // Return the context provider with the value of cache and setError
   return (
     <AppContext.Provider value={{ setError, cache }}>
-      {error ? (
+      {/* Comment out the error display logic for now */}
+      {/* {error ? (
         <div className="RampError">
           <h1 className="RampTextHeading--l">Oops. Application broken</h1>
           <div className="RampBreak--l" />
           Error: {error}
         </div>
-      ) : (
-        children
-      )}
+      ) : ( */}
+        {/* Render the children components */}
+        {children}
+      {/* )} */}
     </AppContext.Provider>
-  )
-}
+  );
+};
+
+// Export the AppContextProvider component
+export { AppContextProvider };
