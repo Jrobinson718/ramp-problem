2fc383e317c4c96cb90c19f8913687e210d8dad1
joro2238 <131008863+Jrobinson718@users.noreply.github.com>
2023-08-27 20:49:26 +0000

    Update index.tsx

--- ramp challenge/public/src/components/AppContextProvider/index.tsx
+++ ramp challenge/public/src/components/AppContextProvider/index.tsx
@@ -1,15 +1,10 @@
 // Import necessary modules and components
 import React, { useRef, useState } from "react";
-// Import AppContext from the appropriate path
-// import { AppContext } from "../../utils/context";
-// Import the necessary type for AppContextProviderComponent
-// import { AppContextProviderComponent } from "./types";
-
-// Create a basic context object since we're not using the actual context
-const AppContext = React.createContext(null);
+import { AppContext } from "../../utils/context"; // Import AppContext from the appropriate path
+import { AppContextProviderComponent } from "./types"; // Import the necessary type for AppContextProviderComponent
 
 // Define the AppContextProvider component
-const AppContextProvider = ({ children }) => {
+export const AppContextProvider: AppContextProviderComponent = ({ children }) => {
   // Create a ref to store a cache map
   const cache = useRef(new Map());
 
@@ -19,20 +14,17 @@ const AppContextProvider = ({ children }) => {
   // Return the context provider with the value of cache and setError
   return (
     <AppContext.Provider value={{ setError, cache }}>
-      {/* Comment out the error display logic for now */}
-      {/* {error ? (
+      {/* Conditionally render error message */}
+      {error ? (
         <div className="RampError">
           <h1 className="RampTextHeading--l">Oops. Application broken</h1>
           <div className="RampBreak--l" />
           Error: {error}
         </div>
-      ) : ( */}
-        {/* Render the children components */}
-        {children}
-      {/* )} */}
+      ) : (
+        /* Render the children components */
+        children
+      )}
     </AppContext.Provider>
   );
-};
-
-// Export the AppContextProvider component
-export { AppContextProvider };
+}
